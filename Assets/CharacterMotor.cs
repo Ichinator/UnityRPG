074c4c3cffee9cef85ddb0d9186730f7f6d7794f
Ichinator <arnolddouwma@orange.fr>
2018-04-03 23:19:50 +0200

    step 2 https://www.youtube.com/watch?v=-DQrS_RiEqs&list=PLUWxWDlz8PYIuMKQ3rXewg2H__jF-92W1&index=3

---
+++ Assets/CharacterMotor.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMotor : MonoBehaviour {
+
+	Animation animations;
+
+	public float walkSpeed;
+	public float runSpeed;
+	public float turnSpeed;
+
+	public string inputFront;
+	public string inputBack;
+	public string inputLeft;
+	public string inputRight;
+
+	public Vector3 jumpSpeed;
+
+	CapsuleCollider playerCollider;
+
+	// Use this for initialization
+	void Start () {
+		animations = gameObject.GetComponent<Animation>();
+		playerCollider = gameObject.GetComponent<CapsuleCollider>();
+
+	}
+
+	bool IsGrounded(){
+		return Physics.CheckCapsule(playerCollider.bounds.center, new Vector3(playerCollider.bounds.center.x, playerCollider.bounds.min.y - 0.1f, playerCollider.bounds.center.z), 0.11f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(inputFront)&& !Input.GetKey(KeyCode.LeftShift)){
+			transform.Translate(0, 0, walkSpeed * Time.deltaTime);
+			animations.Play("walk");
+		}
+
+		if(Input.GetKey(inputFront) && Input.GetKey(KeyCode.LeftShift)){
+			transform.Translate(0, 0, runSpeed * Time.deltaTime);
+			animations.Play("run");
+		}
+
+		if(Input.GetKey(inputBack)){
+			transform.Translate(0, 0, -(walkSpeed)/2 * Time.deltaTime);
+			animations.Play("walk");
+		}
+
+		if(Input.GetKey(inputLeft)){
+			transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+		}
+
+		if(Input.GetKey(inputRight)){
+			transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+		}
+
+		if (!Input.GetKey(inputFront) && !Input.GetKey(inputBack)){
+			animations.Play("idle");
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space) && IsGrounded()){
+			Vector3 v = gameObject.GetComponent<Rigidbody>().velocity;
+			v.y = jumpSpeed.y;
+
+			gameObject.GetComponent<Rigidbody>().velocity = jumpSpeed;
+		}
+
+	}
+}
